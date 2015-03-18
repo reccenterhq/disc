b70b2f3ffe28cdd641f6bcb449c749c310efd697
Patrick Schmitz <pat@reccenter.com>
2015-03-18 15:21:29 -0700

    add find method to topic object

--- lib/disc/topic.rb
+++ lib/disc/topic.rb
@@ -1,4 +1,12 @@
 module Disc
   class Topic < DiscObject
+    root :post_stream
+    list :posts
+
+    def self.find(id)
+      response = Disc.request(:get, "/t/#{id}.json")
+      self.new(response[:body])
+    end
+
   end
 end
--- spec/disc/topic_spec.rb
+++ spec/disc/topic_spec.rb
@@ -7,4 +7,29 @@ describe Disc::Topic do
     expect(subject.title).to eq "Test topic #1"
   end
 
+  context "api requests" do
+    subject { described_class }
+
+    before do
+      Disc.api_url = "http://localhost:3000"
+      Disc.api_key = "key"
+      Disc.api_username = "username"
+
+      stub_get("http://localhost:3000/t/83.json?api_key=key&api_username=username").to_return(
+        body: fixture("topic.json"),
+        headers: { content_type: "application/json" })
+    end
+
+    it "requests the correct resource" do
+      subject.find(83)
+      expect(a_get("http://localhost:3000/t/83.json?api_key=key&api_username=username")).to have_been_made
+    end
+
+    it "returns the requested topics" do
+      response = subject.find(83)
+      expect(response).to be_a Disc::Topic
+      expect(response.first).to be_a Disc::Post
+    end
+  end
+
 end
